2bee99c26ad69a1f3c61955d59ccfdd31f56d6f9
will <williancosta.amaral@gmail.com>
2017-05-15 11:23:23 -0300

    Add Solution

---
+++ Aula1505/Aula1505/Models/Projeto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Aula1505.Models
+{
+    public class Projeto
+    {
+        [Key]
+        public int ProjetoID { get; set; }
+        [Required,StringLength(20)]
+        public string Nome { get; set; }
+        [Display(Name ="Descrição"),DataType(DataType.MultilineText)]
+        public string Descricao { get; set; }
+        public bool Ativo { get; set; }
+    }
+}

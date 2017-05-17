a76932e572d4180371987049ca29e1b4b9c85365
will <williancosta.amaral@gmail.com>
2017-05-17 09:38:00 -0300

    Add novo Projeto

---
+++ Aula1705/Aula1705/WebForm2.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Aula1705
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int qntCliquesViewState = Convert.ToInt32(ViewState["qtdCliques"]);
+            int qntCliquesSession = Convert.ToInt32(Session["QtdCliquesSession"]);
+            lblQuantidadeCliques.Text = qntCliquesSession.ToString();
+        }
+    }
+}

abeb24a7d5b641590cbfc2433ef1b7af39e7b383
will <williancosta.amaral@gmail.com>
2017-06-12 09:46:18 -0300

    Add por Class

--- Aula1206/Aula1206/Controllers/CategoriasController.cs
+++ Aula1206/Aula1206/Controllers/CategoriasController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Aula1206.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,14 +12,22 @@ namespace Aula1206.Controllers
         // GET: Categorias
         public ActionResult Index()
         {
-            List<string> categorias = new List<string>();
-            categorias.Add("Carros");
-            categorias.Add("Motos");
-            categorias.Add("Barcos");
-            categorias.Add("Aviões");
-            categorias.Add("Caminhões");
+            List<Categoria> categorias = new List<Categoria>();
+
+            categorias.Add(new Categoria() { Nome = "Carros" });
+            categorias.Add(new Categoria() { Nome = "Motos" });
+            categorias.Add(new Categoria() { Nome = "Barcos" });
+            categorias.Add(new Categoria() { Nome = "Aviões" });
+            categorias.Add(new Categoria() { Nome = "Caminhões" });
 
             ViewBag.ListaCategorias = categorias;
+
+            return View(categorias);
+        }
+
+        //GET
+        public ActionResult Create()
+        {
             return View();
         }
     }
---
+++ Aula1206/Aula1206/Models/Categoria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Aula1206.Models
+{
+    public class Categoria
+    {
+        public int CategoriaID { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
